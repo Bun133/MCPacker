fba04d472a7c1237f3c8ef7ddf84cd69c395a8f4
bun133 <bun133.work@gmail.com>
2022-11-03 17:26:53 +0900

    [Test] Add Test Case

---
+++ main/src/test/java/com/github/bun133/mcpacker/PackTest.kt
@@ -0,0 +1,55 @@
+package com.github.bun133.mcpacker
+
+import com.github.bun133.mcpacker.defaultPacker.DataPackPackerEntry
+import org.junit.Test
+import java.io.File
+import java.nio.file.Paths
+
+class PackTest {
+    private val testTopDir = Paths.get("./src/test/resources/test").toFile()
+    private val resultTopDir = Paths.get("./src/test/resources/test/result").toFile()
+    private val expectedTopDir = Paths.get("./src/test/resources/test/expected").toFile()
+
+    @Test
+    fun generateBaseResourcePack() {
+        runTestWithName("base", listOf(DataPackPackerEntry(description = "Test")))
+    }
+
+    private fun runTestWithName(name: String, entries: List<PackEntry<*>>) {
+        val targetDir = File(resultTopDir, name)
+        if (targetDir.exists()) {
+            targetDir.deleteRecursively()
+        }
+        assertAndPack(targetDir, entries)
+    }
+
+    private fun assertAndPack(targetDir: File, entries: List<PackEntry<*>>): Boolean {
+        val b = pack(targetDir, entries)
+        assert(b) { "generate Resource Pack" }
+        assertSame(targetDir, File(expectedTopDir, targetDir.name))
+        return b
+    }
+
+    private fun assertSame(targetDir: File, expectedDir: File) {
+        assert(isSame(targetDir, expectedDir)) { "generated Data pack is not same as expected!" }
+    }
+
+    private fun isSame(directory1: File, directory2: File): Boolean {
+        if (!directory1.exists() || !directory2.exists()) return false
+        if (directory1.isDirectory && directory2.isDirectory) {
+            val list1 = directory1.listFiles() ?: return false
+            val list2 = directory2.listFiles() ?: return false
+            if (list1.size != list2.size) {
+                return false
+            }
+            for (i in list1.indices) {
+                if (!isSame(list1[i], list2[i])) {
+                    return false
+                }
+            }
+            return true
+        } else {
+            return directory1.readBytes().contentEquals(directory2.readBytes())
+        }
+    }
+}
